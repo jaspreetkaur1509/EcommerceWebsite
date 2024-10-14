203a5e2a98d5574055e4955f6d158117901daef2
Jaspreet Kaur <kaurjaspreet0915@gmail.com>
2024-10-14 20:53:41 +0530

    initial stage 0

--- Frontened/src/Pages/LoginSignup.jsx
+++ Frontened/src/Pages/LoginSignup.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div className='LoginSignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your Name' />
+          <input type="email" placeholder='Email Address' />
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p></p>
+      </div>
+    </div>
   )
 }
 
