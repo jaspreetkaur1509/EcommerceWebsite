9cdbd4da4b8ffa561b82960f4c84d57fe9fb9273
Jaspreet Kaur <kaurjaspreet0915@gmail.com>
2024-10-16 20:37:58 +0530

    initial stage 0

--- Frontened/src/Context/ShopContext.jsx
+++ Frontened/src/Context/ShopContext.jsx
@@ -21,11 +21,37 @@ const ShopContextProvider = (props) => {
         console.log(cartItems);
     }
 
-    const removeToCart = (itemId) => {
+    const removeFromCart = (itemId) => {
         setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}))
     }
 
-    const contextValue = {all_product, cartItems, addToCart, removeToCart};
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for(const item in cartItems)
+            {
+                if(cartItems[item]>0)
+                {
+                let itemInfo = all_product.find((product) =>product.id===Number(item));
+                if(itemInfo){
+                    totalAmount += itemInfo.new_price * cartItems[item];
+                 }
+            }
+        }
+            return totalAmount;
+    }
+
+    const getTotalCartItems = () => {
+        let totalItem = 0;
+        for(const item in cartItems)
+        {
+            if(cartItems[item]>0){
+                totalItem+= cartItems[item];
+            }
+        }
+        return totalItem;
+    }
+
+    const contextValue = {getTotalCartItems,getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart};
 
 
     return (
